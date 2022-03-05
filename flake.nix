0d2bc6c01051e9ff5eee24fc78af8644ef872119
Philipp Herzog <philipp.herzog@protonmail.com>
2022-03-05 01:56:53 +0100

    update: lockfile, remove old fix to get rpi4 to boot

--- flake.nix
+++ flake.nix
@@ -9,8 +9,6 @@
       inherit system;
     };
     lib = nixpkgs.lib;
-    flash = pkgs.writeShellScriptBin "flash" ''
-    '';
   in rec {
     devShell.${system} = pkgs.mkShell {
       buildInputs = with pkgs; [
@@ -57,53 +55,9 @@
       ];
     };
 
-
-    nixosConfigurations.rpi4-new = lib.nixosSystem {
-      system = "aarch64-linux";
-
-      modules = [
-        {
-          imports = [
-            # https://nixos.wiki/wiki/NixOS_on_ARM#Build_your_own_image
-            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
-          ];
-
-          services.openssh = {
-            enable = true;
-            permitRootLogin = "yes";
-          };
-          users.extraUsers.root.initialPassword = lib.mkForce "test123";
-
-          # https://github.com/NixOS/nixpkgs/issues/135828
-          hardware.deviceTree.overlays = [
-            {
-              # https://github.com/NixOS/nixpkgs/issues/135828#issuecomment-918359063
-              name = "issuecomment-918359063";
-              dtsText = ''
-              // SPDX-License-Identifier: GPL-2.0
-              /dts-v1/;
-              /plugin/;
-              / {
-                compatible = "brcm,bcm2711";
-                fragment@1 {
-                  target = <&emmc2bus>;
-                  __overlay__ {
-                    dma-ranges = <0x00 0x00 0x00 0x00 0xfc000000>;
-                  };
-                };
-              };
-              '';
-            }
-          ];
-        }
-      ];
-    };
-
-
     images = {
       rpi2 = nixosConfigurations.rpi2.config.system.build.sdImage;
       rpi4 = nixosConfigurations.rpi4.config.system.build.sdImage;
-      rpi4-new = nixosConfigurations.rpi4-new.config.system.build.sdImage;
     };
   };
 }
